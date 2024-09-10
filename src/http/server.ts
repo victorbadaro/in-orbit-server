dac6aa3ada36144354865c3e48b4a25630678b50
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-10 14:35:29 -0300

    feat: add routes to list pending goals and complete goals

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,12 +6,36 @@ import {
 } from 'fastify-type-provider-zod';
 import z from 'zod';
 import { createGoal } from '../functions/create-goal';
+import { createGoalCompletion } from '../functions/create-goal-completion';
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+app.get('/pending-goals', async () => {
+	const { pendingGoals } = await getWeekPendingGoals();
+
+	return { pendingGoals };
+});
+app.post(
+	'/completions',
+	{
+		schema: {
+			body: z.object({
+				goalId: z.string()
+			})
+		}
+	},
+	async (request) => {
+		const { goalId } = request.body;
+
+		await createGoalCompletion({
+			goalId
+		});
+	}
+);
 app.post(
 	'/goals',
 	{
@@ -22,7 +46,7 @@ app.post(
 			})
 		}
 	},
-	async (request, reply) => {
+	async (request) => {
 		const { title, desiredWeeklyFrequency } = request.body;
 
 		await createGoal({
