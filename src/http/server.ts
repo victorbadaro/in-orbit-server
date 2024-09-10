fe7a4512b662a47c24225dec259b4b3f758f45df
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-10 02:24:12 -0300

    feat: add route to create a goal

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify';
+import z from 'zod';
+import { createGoal } from '../functions/create-goal';
 
 const app = fastify();
+
+app.post('/goals', async (request, reply) => {
+	const createGoalSchema = z.object({
+		title: z.string(),
+		desiredWeeklyFrequency: z.number().int().min(1).max(7)
+	});
+
+	const body = createGoalSchema.parse(request.body);
+
+	await createGoal({
+		title: body.title,
+		desiredWeeklyFrequency: body.desiredWeeklyFrequency
+	});
+});
 
 app
 	.listen({ port: 3000 })
