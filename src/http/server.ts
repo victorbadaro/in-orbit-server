dc1fae915190092c18791edf68d7ce0e996158a2
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-10 22:53:12 -0300

    refactor: separate routes in different files

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,57 +4,18 @@ import {
 	serializerCompiler,
 	validatorCompiler
 } from 'fastify-type-provider-zod';
-import z from 'zod';
-import { createGoal } from '../functions/create-goal';
-import { createGoalCompletion } from '../functions/create-goal-completion';
-import { getWeekPendingGoals } from '../functions/get-week-pending-goals';
+import { createCompletionRoute } from './routes/create-completion';
+import { createGoalRoute } from './routes/create-goal';
+import { getPendingGoalsRoute } from './routes/get-pending-goals';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.get('/pending-goals', async () => {
-	const { pendingGoals } = await getWeekPendingGoals();
-
-	return { pendingGoals };
-});
-app.post(
-	'/completions',
-	{
-		schema: {
-			body: z.object({
-				goalId: z.string()
-			})
-		}
-	},
-	async (request) => {
-		const { goalId } = request.body;
-
-		await createGoalCompletion({
-			goalId
-		});
-	}
-);
-app.post(
-	'/goals',
-	{
-		schema: {
-			body: z.object({
-				title: z.string(),
-				desiredWeeklyFrequency: z.number().int().min(1).max(7)
-			})
-		}
-	},
-	async (request) => {
-		const { title, desiredWeeklyFrequency } = request.body;
-
-		await createGoal({
-			title,
-			desiredWeeklyFrequency
-		});
-	}
-);
+app.register(createGoalRoute);
+app.register(createCompletionRoute);
+app.register(getPendingGoalsRoute);
 
 app
 	.listen({ port: 3000 })
