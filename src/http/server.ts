12b79e6a831e78cdf2997ed9b2609f8b26e6d68d
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-10 23:38:57 -0300

    feat: add summary route

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,6 +7,7 @@ import {
 import { createCompletionRoute } from './routes/create-completion';
 import { createGoalRoute } from './routes/create-goal';
 import { getPendingGoalsRoute } from './routes/get-pending-goals';
+import { getWeekSummaryRoute } from './routes/get-week-summary';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -16,6 +17,7 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createGoalRoute);
 app.register(createCompletionRoute);
 app.register(getPendingGoalsRoute);
+app.register(getWeekSummaryRoute);
 
 app
 	.listen({ port: 3000 })
