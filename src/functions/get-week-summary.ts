6ace7f63e8da574b27ae39285c551b6069e177f5
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-13 17:35:48 -0300

    chore: reorder weekday summary request result

--- src/functions/get-week-summary.ts
+++ src/functions/get-week-summary.ts
@@ -45,6 +45,7 @@ export async function getWeekSummary() {
 					lte(goalCompletions.createdAt, lastDayOfWeek)
 				)
 			)
+			.orderBy(desc(goalCompletions.createdAt))
 	);
 
 	const goalsCompletedByWeekDay = db.$with('goals_completed_by_week_day').as(
